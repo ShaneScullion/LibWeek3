94dc551a77febbc81adde01f39283d010daa9613
B00984034 <B00984034@M-MXB105-C63>
2025-10-06 15:28:14 +0100

    Encap

--- Libweek3/Book.cs
+++ Libweek3/Book.cs
@@ -1,23 +1,41 @@
-﻿using System;
-
-namespace Library
+﻿namespace Library
 {
-   public class Book
+    class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title; // private variable
+        private string author;// private variable
+        private string isbn; // private variable
 
-     public Book(string bookTitle, string bookAuthor, string bookISBN)
+        // Title property allows access
+        // to the title private variable
+        public string Title
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            get { return title; }  // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
         }
 
-       public void DisplayInfo()
+        // Constructor to add a new book
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
-            Console.WriteLine($"Book title: {Title}");
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
+        }
+
+        // Method to display information about a book
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book Title: {Title}");
             Console.WriteLine($"Book Author: {Author}");
             Console.WriteLine($"Book ISBN: {ISBN}");
             Console.WriteLine();
